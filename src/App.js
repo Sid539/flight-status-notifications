3db18cb49a42761a6e465637783c310ec4740577
Sidhant Tyagi <111180054+Sid539@users.noreply.github.com>
2024-07-27 11:40:42 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FlightStatus from './components/FlightStatus';
+import Notifications from './components/Notifications';
+
+function App() {
+  return (
+    <div className="App">
+      <FlightStatus />
+      <Notifications />
+    </div>
+  );
+}
+
+export default App;
