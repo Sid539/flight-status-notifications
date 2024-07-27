89bb895074eefb9e8d7599aba90983eadd99807c
Sidhant Tyagi <111180054+Sid539@users.noreply.github.com>
2024-07-27 11:23:36 +0530

    Update Flight.java

--- model/Flight.java
+++ model/Flight.java
@@ -1,16 +1,36 @@
 package com.example.flightstatus.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Flight {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String flightNumber;
+    private String status;
 
-    private String number;
+    public Long getId() {
+        return id;
+    }
 
-    @Enumerated(EnumType.STRING)
-    private FlightStatus status;
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
